7525d39fa009f0a2f3327eaa6e44205d53662b66
Aman Vishwakarma <aman.vishwakarma@coriolis.co.in>
2023-07-29 21:16:09 +0530

    Added logic for getting dubbed audio file from 11labs

--- DubMatic/views.py
+++ DubMatic/views.py
@@ -37,11 +37,13 @@ def postVideo(request):
             # Extracting text
             text=speechToText(outputWavPath)
 
-            translate_to_hindi(text,outputWavPath)
+            hindiTextFile=translate_to_hindi(text,outputWavPath)
 
             # Add voice in 11labs
-            addVoice(request,outputWavPath)
+            voice_id=addVoice(request,outputWavPath)
 
+            # Giving text to 11 labs
+            addText11(request,hindiTextFile,voice_id)
             return JsonResponse({'videoPath':videoPath,'lang':lang})
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'message': 'Invalid JSON format'})
@@ -79,11 +81,11 @@ def wavConvert(inputPath, outputPath):
 def translate_to_hindi(text,filePath):
     translator = Translator()
     translated_text = translator.translate(text, src='en', dest='hi')
-    print(translated_text.text)
     # saving text in a file
     filePath=filePath[0:-4]+"hindi.txt"
     with open(filePath, 'w',encoding='utf-8') as file:
         file.write(translated_text.text)
+    return filePath
     # return translated_text.text
 
 def speechToText(filePath):
@@ -96,6 +98,7 @@ def speechToText(filePath):
 
 # 11 labs functions
 def addVoice(request,wavPath):
+
     url = "https://api.elevenlabs.io/v1/voices/add"
 
     headers = {
@@ -115,3 +118,41 @@ def addVoice(request,wavPath):
 
     response = requests.post(url, headers=headers, data=data, files=files)
     print(response.text)
+    if response.status_code == 200:
+        # Parse the response JSON and access the 'voice_id' attribute
+        response_data = response.json()
+        voice_id = response_data.get('voice_id')
+        print("Voice ID:", voice_id)
+        return voice_id
+    else:
+        print("Error: Request failed with status code", response.status_code)
+        return None
+
+def addText11(request,filePathHindi,voice_id):
+    CHUNK_SIZE = 1024
+    url = "https://api.elevenlabs.io/v1/text-to-speech/"+voice_id+"/stream"
+
+    headers = {
+    "Accept": "audio/mpeg",
+    "Content-Type": "application/json",
+    "xi-api-key": "fc3395a73afe62ac77c4099589cc5e85"
+    }
+
+    with open(filePathHindi,'r',encoding="utf-8") as file:
+        contents=file.read()
+
+    data = {
+    "text": contents,
+    "model_id": "eleven_multilingual_v1",
+    "voice_settings": {
+        "stability": 1,
+        "similarity_boost": 1
+    }
+    }
+    print(data)
+    response = requests.post(url, json=data, headers=headers)
+    hindiAudio=filePathHindi[0:-4]+".mp3"
+    with open(hindiAudio, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
