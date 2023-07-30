079f3fd98a14deeb192eb7224fae037036fea042
Aman Vishwakarma <vishwakarmaaman619@gmail.com>
2023-07-30 17:04:35 +0530

    Added logic for adding dubbed voice in video

--- DubMatic/views.py
+++ DubMatic/views.py
@@ -10,7 +10,13 @@ import requests
 import assemblyai as aai
 from googletrans import Translator
 import whisper
-
+from pydub import AudioSegment
+from pydub.playback import play
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
+import subprocess
+from moviepy.editor import VideoFileClip, AudioFileClip
+from moviepy.audio.fx import audio_loop
+import os
 
 @csrf_protect
 @csrf_exempt
@@ -25,25 +31,42 @@ def postVideo(request):
             lang=data["lang"]
 
             # Extracting audio
-            outputPath=videoPath[0:len(videoPath)-4]+".mp3"
-            extractAudio(videoPath,outputPath)
-            print(outputPath)
+            audioMp3=videoPath[0:len(videoPath)-4]+".mp3"
+            extractAudio(videoPath,audioMp3)
+            # print(outputPath)
 
             outputWavPath=videoPath[0:len(videoPath)-4]+".wav"
-            inputWavPath=outputPath
+            inputWavPath=audioMp3
 
-            wavConvert(inputWavPath,outputWavPath)            
+            wavConvert(inputWavPath,outputWavPath)   
+            print("audio extracted")
 
             # Extracting text
             text=speechToText(outputWavPath)
+            print("text extracted")
 
-            hindiTextFile=translate_to_hindi(text,outputWavPath)
+            translatedTextFile=translateTextToTargetLang(text,outputWavPath,lang)
+            print("text converted")
 
-            # Add voice in 11labs
+            # # Add voice in 11labs
             voice_id=addVoice(request,outputWavPath)
+            print("voice added to 11 labs")
 
-            # Giving text to 11 labs
-            addText11(request,hindiTextFile,voice_id)
+            # # Giving text to 11 labs
+            translatedAudioPath=addText11(request,translatedTextFile,voice_id)
+            print("audio retrieved from 11 labs")
+
+            # addaudio to video
+            translatedVideoPath=translatedAudioPath[0:-4]+".mp4"
+            replace_audio_in_video(videoPath,translatedAudioPath,translatedVideoPath) 
+            print("audio added to video")
+            os.remove(audioMp3) 
+            os.remove(outputWavPath) 
+            os.remove(translatedTextFile) 
+            os.remove(translatedAudioPath) 
+
+
+            
             return JsonResponse({'videoPath':videoPath,'lang':lang})
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'message': 'Invalid JSON format'})
@@ -65,24 +88,24 @@ def extractAudio(videoPath, outputPath):
         audioClip.close()
         videoClip.close()
 
-        print("Audio extraction successful.")
+        # print("Audio extraction successful.")
     except Exception as e:
         print("Error extracting audio:", str(e))
 
 def wavConvert(inputPath, outputPath):
 
-    print(inputPath,outputPath)
+    # print(inputPath,outputPath)
     # Load the MP3 file using pydub
     audio = AudioSegment.from_mp3(inputPath)
-
     # Export the audio as a WAV file
     audio.export(outputPath, format='wav')
+    
 
-def translate_to_hindi(text,filePath):
+def translateTextToTargetLang(text,filePath,target_lang):
     translator = Translator()
-    translated_text = translator.translate(text, src='en', dest='hi')
+    translated_text = translator.translate(text, src='en', dest=target_lang)
     # saving text in a file
-    filePath=filePath[0:-4]+"hindi.txt"
+    filePath=filePath[0:-4]+"translate.txt"
     with open(filePath, 'w',encoding='utf-8') as file:
         file.write(translated_text.text)
     return filePath
@@ -91,9 +114,9 @@ def translate_to_hindi(text,filePath):
 def speechToText(filePath):
     model = whisper.load_model("base.en")
     result = model.transcribe(filePath)
-    filePath=filePath[0:-4]+".txt"
-    with open(filePath, 'w') as file:
-        file.write(result['text'])
+    # filePath=filePath[0:-4]+".txt"
+    # with open(filePath, 'w') as file:
+    #     file.write(result['text'])
     return result['text']
 
 # 11 labs functions
@@ -108,7 +131,7 @@ def addVoice(request,wavPath):
 
     data = {
         'name': 'morgan',
-        'labels': '{"accent": "American"}',
+        'labels': '{"accent": "Indian"}',
         'description': 'Voice description'
     }
 
@@ -117,19 +140,19 @@ def addVoice(request,wavPath):
     ]
 
     response = requests.post(url, headers=headers, data=data, files=files)
-    print(response.text)
+    # print(response.text)
     if response.status_code == 200:
         # Parse the response JSON and access the 'voice_id' attribute
         response_data = response.json()
         voice_id = response_data.get('voice_id')
-        print("Voice ID:", voice_id)
+        # print("Voice ID:", voice_id)
         return voice_id
     else:
         print("Error: Request failed with status code", response.status_code)
         return None
 
 def addText11(request,filePathHindi,voice_id):
-    CHUNK_SIZE = 1024
+    CHUNK_SIZE = 2048
     url = "https://api.elevenlabs.io/v1/text-to-speech/"+voice_id+"/stream"
 
     headers = {
@@ -149,10 +172,17 @@ def addText11(request,filePathHindi,voice_id):
         "similarity_boost": 1
     }
     }
-    print(data)
+    # print(data)
     response = requests.post(url, json=data, headers=headers)
-    hindiAudio=filePathHindi[0:-4]+".mp3"
-    with open(hindiAudio, 'wb') as f:
+    translated_audio=filePathHindi[0:-4]+".mp3"
+    with open(translated_audio, 'wb') as f:
         for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
             if chunk:
                 f.write(chunk)
+    return translated_audio
+
+def replace_audio_in_video(video_path, audio_path, output_path):
+    video_clip = VideoFileClip(video_path)
+    audio_clip = AudioFileClip(audio_path)
+    video_with_new_audio = video_clip.set_audio(audio_clip)
+    video_with_new_audio.write_videofile(output_path, codec="libx264", audio_codec="aac") 
